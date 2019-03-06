046c7b17838711219499be75960e81477186257f
shaozh <shaozh@bw30.com>
2019-03-06 10:33:42 +0800

    1903061032

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -9,17 +7,11 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://newbieHui.github.io
 git push -f git@github.com:newbieHui/newbieHui.github.io.git master
-
-# 如果发布到 https://newbieHui.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:newbieHui/<REPO>.git master:gh-pages
 
 cd -
