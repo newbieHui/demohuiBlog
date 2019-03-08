56c95f53196aa945cf8caec8a9555ffc2b060c7a
shaozh <shaozh@bw30.com>
2019-03-08 10:39:22 +0800

    1903081038

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,12 +10,13 @@ module.exports = {
     dest: './docs/.vuepress/dist',   // 设置输出目录
     port: 8080, //端口
     themeConfig: { //主题配置
+        logo: '/img/homePage.jpg',
         // 添加导航栏
         nav: [
             { text: '主页', link: '/' }, // 导航条
             { text: '前端库', link: '/webLibrary/' },
-            { text: '项目库', link: '/notes/' },
-            { text: '随笔库', link: '/essay/' },
+            { text: '项目库', link: '/projectLibrary/' },
+            { text: '随笔库', link: '/essayLibrary/' },
             { text: 'github',link:'https://github.com/newbieHui'}
         ],
         // 为以下路由添加侧边栏
@@ -32,15 +33,47 @@ module.exports = {
                 },
                 {
                     title: 'Vue',
-                    collapsable: false,
+                    collapsable: true,
                     children: []
                 },
                 {
                     title: 'Element-ui',
-                    collapsable: false,
+                    collapsable: true,
                     children: []
                 },
             ],
+            '/projectLibrary/': [
+                {
+                    title: 'Vue项目',
+                    collapsable: true,
+                    children: [
+                        'childrenLibrary/translateOnline'
+                    ]
+                },
+            ],
+            '/essayLibrary/': [
+                {
+                    title: 'Book',
+                    collapsable: true,
+                    children: [
+                        'childrenLibrary/book1'
+                    ]
+                },
+                {
+                    title: 'Movie',
+                    collapsable: true,
+                    children: [
+                        'childrenLibrary/movie1'
+                    ]
+                },
+                {
+                    title: 'Music',
+                    collapsable: true,
+                    children: [
+                        'childrenLibrary/music1'
+                    ]
+                },
+            ]
         }
     }
 };
